d97e7938dd5f07664bc95325edfbc6de455832fa
chenhaonan <942509332@qq.com>
2022-12-05 11:41:53 +0800

    updata task time

--- spider/task.go
+++ spider/task.go
@@ -1,8 +1,8 @@
 package spider
 
-import (
-	cron "github.com/robfig/cron/v3"
-)
+import cron "github.com/robfig/cron/v3"
+
+var taskMap map[string][]func()
 
 func StartTask() {
 	crontab := cron.New()
@@ -21,14 +21,20 @@ func StartTask() {
 		CollyBondZhUsRate()
 	}
 
-	// 月初第一天10点半
-	crontab.AddFunc("0 15 10 1 *", func() { CollyCNPMI() })
-	//每月9-15日上午10:15触发
-	crontab.AddFunc("0 15 10 9-15 *", task)
+	// 月初第一天10点
+	crontab.AddFunc("0 10 1 * *", func() { CollyCNPMI() })
+	//每月9-15日上午10:00触发
+	crontab.AddFunc("0 10 9-15 * *", task)
 	// 每日9点
-	crontab.AddFunc("0 15 9 * *", FuturesForeignTask)
+	crontab.AddFunc("0 9 * * *", FuturesForeignTask)
 	//1月、4月、7月、10月17日-19 9点半
-	crontab.AddFunc("0 15 9 17-19 1,4,7,10", func() { CollyCNGDP() })
+	crontab.AddFunc("0 9 17-19 1,4,7,10 *", func() { CollyCNGDP() })
+
+	for k, v := range taskMap {
+		for _, f := range v {
+			crontab.AddFunc(k, f)
+		}
+	}
 	// 启动定时器
 	crontab.Start()
 }
